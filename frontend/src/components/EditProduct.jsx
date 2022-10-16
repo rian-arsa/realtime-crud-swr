569195b9acd80fc418d3a0ee29ab57d973090e58
Rian Ari Sasono <rianono48@gmail.com>
2022-10-16 20:43:04 +0700

    Add FE using tailwind swr

---
+++ frontend/src/components/EditProduct.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
+const EditProduct = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getProductById = async () => {
+      const response = await axios.get(
+        `http://localhost:5000/api/products/${id}`
+      );
+      setName(response.data.name);
+      setPrice(response.data.price);
+    };
+    getProductById();
+  }, [id]);
+
+  const saveProduct = async (e) => {
+    e.preventDefault();
+
+    await axios.patch(`http://localhost:5000/api/products/${id}`, {
+      name,
+      price: parseInt(price),
+    });
+    navigate("/");
+  };
+
+  return (
+    <div className="max-w-lg mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300">
+      <h1 className="text-lg font-bold text-center">Add product</h1>
+      <form className="my-10" onSubmit={saveProduct}>
+        <div className="flex flex-col">
+          <div className="mb-5">
+            <label className="font-bold text-slate-700">Product name</label>
+            <input
+              type="text"
+              className="w-full py-3 mt-1 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
+              placeholder="Product name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="mb-5">
+            <label className="font-bold text-slate-700">Price</label>
+            <input
+              type="text"
+              className="w-full py-3 mt-1 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
+              placeholder="Price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full py-3 font-bold text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default EditProduct;
